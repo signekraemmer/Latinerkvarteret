50f150926a11ee863e9b19f996dec6cfbc352c11
Signe Kræmmer <signekraemmer@hotmail.com>
2019-09-16 11:34:32 +0200

    header påbegyndt

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
-var Latiner;
-Latiner ="Første del";
-var Kvarteret;
-Kvarteret ="Sidste del";
+var myIndex = 0;
+carousel();
 
-var Sammen;
-
-Sammen = Latiner + Kvarteret;
-
-console.log(Sammen)
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
