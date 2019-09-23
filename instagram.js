04551cce7f7179a245124870aa988ec7746ccbfe
MathiasGN <math964g@edu.ucl.dk>
2019-09-23 12:20:54 +0200

    Multiple changes
    - Added gitignore
    - Added instagram SoMe

---
+++ instagram.js
@@ -0,0 +1,18 @@
+document.getElementById("post").addEventListener("click", function(){postShow()});
+console.log("After the click event");
+
+function postShow() {
+  console.log("postShow() running");
+  var post = document.getElementById("post");
+
+  if (post.style.opacity == 0) {
+    post.style.opacity = 1;
+    console.log("Turned Visible");
+  }
+
+// Also catches errors, if the opacity value is random
+  else {
+    post.style.opacity = 0;
+    console.log("Turned Invisible");
+  }
+}
