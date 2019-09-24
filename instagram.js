9b7c9bc808c24d5bd56cc8507143111590c653cf
MathiasGN <math964g@edu.ucl.dk>
2019-09-24 15:20:40 +0200

    FIxed the IG issues with multiple functions

--- instagram.js
+++ instagram.js
@@ -1,26 +1,40 @@
-document.getElementById("post").addEventListener("click", postShow);
+console.log("instagram.js loaded...");
+
+document.getElementById("activatepost").addEventListener("click", postShow);
 console.log("After the click event");
 
 function postShow() {
   console.log("postShow() running");
   var post = document.getElementById("post");
 
-  if (post.style.opacity == 0) {
-    post.style.opacity = 1;
+  if (post.style.display == "none") {
+    post.style.display = "block";
     console.log("Turned Visible");
   }
 
-// Also catches errors, if the opacity value is random
+  // Also catches errors, if the opacity value is random
   else {
-    post.style.opacity = 0;
+    post.style.display = "none";
     console.log("Turned Invisible");
   }
 }
 
+document.getElementById("post").addEventListener("click", postHide);
+
+function postHide() {
+  var post = document.getElementById("post");
+  post.style.display = "none";
+
+}
+
+
+
 
 
 // InstaStory
-document.getElementsByClassName("activatebutton").addEventListener("click", showSlides);
+var activatebtn = document.getElementsByClassName("activatebutton");
+activatebtn[0].addEventListener("click", showSlides);
+
 var slideIndex = 0;
 
 function showSlides() {
@@ -41,6 +55,6 @@ function showSlides() {
   }
 
   // Makes a slide visible
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 1000); // Change image every 4 seconds
 }
