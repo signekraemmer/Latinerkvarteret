e93e35adcb2de1a17682f9e71e094d54ac67ee98
MathiasGN <43538343+math964g@users.noreply.github.com>
2019-09-25 19:30:44 +0200

    bh

--- navbar.js
+++ navbar.js
@@ -1,22 +1,8 @@
-// const navSlide = () => {
-//   const burger = document.querySelector(".burger");
-//   const nav = document.querySelector(".navlinks");
-// console.log("variables loaded successfully");
-//
-//   burger.addEventListener("click", () => {
-//     nav.classList.toggle("nav-active");
-//     console.log("Burger clicked");
-//   });
-// }
-//
-// navSlide();
-// console.log("Burger click executed");
-
-var burger = document.getElementsByClassName("burger")
-burger.addEventListener("click", navSlideDown);
+var burger = document.getElementsByClassName("burger");
+burger[0].addEventListener("click", navSlideDown);
 
 function navSlideDown() {
   var nav = document.getElementsByClassName("navlinks");
-  nav.classList.toggle("nav-active");
+  nav.classList.toggle("navactive");
   console.log("Own Slider function executed");
 }
