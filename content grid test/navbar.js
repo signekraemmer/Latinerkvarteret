9c851cff6927e31fca29a77c209bc87c5c176275
MathiasGN <43538343+math964g@users.noreply.github.com>
2019-09-26 20:52:16 +0200

    Added headline row

--- content grid test/navbar.js
+++
@@ -1,8 +0,0 @@
-var burger = document.getElementsByClassName("burger");
-burger[0].addEventListener("click", navSlideDown);
-
-function navSlideDown() {
-  var nav = document.getElementsByClassName("navlinks");
-  nav[0].classList.toggle("navactive");
-  console.log("Own Slider function executed");
-}
